bc6d362e68ad1a484cbb6c940da3f6329b7204a1
Miroslav Mamrák <miroslav.mamrak@seznam.cz>
2020-12-03 17:42:20 +0100

    Day 1 part 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -6,6 +6,22 @@ import java.nio.file.Paths
 
 object Day1 {
     @JvmStatic
+    fun part1() {
+        val pathToData = Paths.get(File("").absolutePath, "src/main/resources/aoc1.data")
+        val data = readDataFileInput(pathToData)
+        val dataLastIndex = data.size - 1
+        for (i in 0..dataLastIndex) {
+            for (j in i + 1..dataLastIndex) {
+                val x = Integer.parseInt(data[i])
+                val y = Integer.parseInt(data[j])
+                if (x + y == 2020) {
+                    println("${x} + ${y} == ${x + y}; multiply is ${x * y}")
+                }
+            }
+        }
+    }
+
+    @JvmStatic
     fun part2() {
         val pathToData = Paths.get(File("").absolutePath, "src/main/resources/aoc1.data")
         val data = readDataFileInput(pathToData)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ object Main {
     fun main(args : Array<String>) {
         val d1 = Day1
         val d2 = Day2
+        d1.part1()
         d1.part2()
         d2.part2()
     }
